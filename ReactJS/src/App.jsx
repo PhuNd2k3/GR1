ec1f3b6cd0689629d03246edce33e9c1bd888c87
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2024-06-02 01:24:01 +0700

    map

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -7,20 +7,20 @@ import { Map } from "./components/Map";
 import { GridImage, DataTable, Home } from "./components";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/data_table" element={<DataTable />} />
-                    <Route path="/grid_image" element={<GridImage />} />
-                    <Route path="/chart" element={<Chart />} />
-                    <Route path="/maze" element={<Maze />} />
-                    <Route path="/map" element={<Map />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/data_table" element={<DataTable />} />
+          <Route path="/grid_image" element={<GridImage />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="/maze" element={<Maze />} />
+          <Route path="/map" element={<Map />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- ReactJS/src/components/Map/Map.jsx
+++ ReactJS/src/components/Map/Map.jsx
@@ -1,44 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import { useMapEvents } from "react-leaflet/hooks";
-import axios from "axios";
+import "leaflet/dist/leaflet.css";
+import "./leaflet-routing-machine-3.2.12/dist/leaflet-routing-machine";
+import "./leaflet-routing-machine-3.2.12/dist/leaflet-routing-machine.css";
+import L from "leaflet";
+import TextField from "@mui/material/TextField";
+
+import startIcon from "../../assets/startIcon.png";
+import endIcon from "../../assets/endIcon.png";
+
 export const Map = () => {
-  const position = [51.505, -0.09];
-  const [start, setStart] = useState("");
-  const [end, setEnd] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-  const [address, setAddress] = useState("");
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+  const [mapCenter, setMapCenter] = useState([21.1812352, 105.84064]);
+  const [isStartSelected, setIsStartSelected] = useState(true);
 
-  const getGeocode = async (lat, lng) => {
-    const url = `https://nominatim.openstreetmap.org/reverse?format=json&amp;lat=${lat}&amp;lon=${lng}`;
+  const handleStartClick = () => {
+    setIsStartSelected(true);
+  };
 
-    try {
-      const response = await axios.get(url);
-      const result = response.data;
-      if (result) {
-        setAddress(result.display_name);
-      } else {
-        setAddress("Address not found");
-      }
-    } catch (error) {
-      console.error(error);
-      setAddress("Error retrieving address");
-    }
+  const handleEndClick = () => {
+    setIsStartSelected(false);
   };
 
   function GetLocation() {
     const map = useMapEvents({
-      click: () => {
-        map.locate();
-      },
-      locationfound: (location) => {
-        console.log(location);
+      click: async (event) => {
+        const { lat, lng } = event.latlng;
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&addressdetails=1&zoom=18`
+        );
+        const data = await response.json();
+        const address = data.display_name.replace(/Xã\s*|Huyện\s*/g, "");
+
+        if (isStartSelected) {
+          setStart({ lat, lng, address });
+        } else {
+          setEnd({ lat, lng, address });
+        }
+        console.log(start, " ", end);
       },
     });
     return null;
   }
+
+  const startMarkerIcon = L.icon({
+    iconUrl: startIcon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+  });
+
+  const endMarkerIcon = L.icon({
+    iconUrl: endIcon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+  });
+
   return (
     <div
       style={{
@@ -55,12 +73,36 @@ export const Map = () => {
           alignContent: "center",
         }}
       >
-        <div>Bắt đầu</div>
-        <div>Kết thúc</div>
+        <div style={{ marginBottom: "30px", padding: "0px 20px" }}>
+          <TextField
+            label="Bắt đầu"
+            variant="outlined"
+            onClick={handleStartClick}
+            fullWidth
+            style={{ width: "calc(100%)" }}
+            value={start ? start.address : ""}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </div>
+        <div style={{ padding: "0px 20px" }}>
+          <TextField
+            label="Kết thúc"
+            variant="outlined"
+            onClick={handleEndClick}
+            fullWidth
+            style={{ width: "calc(100%)" }}
+            value={end ? end.address : ""}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </div>
       </div>
       <div>
         <MapContainer
-          center={position}
+          center={mapCenter}
           zoom={13}
           scrollWheelZoom={false}
           style={{ width: "60vw", height: "100%" }}
@@ -69,7 +111,17 @@ export const Map = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {/* <GetLocation /> */}
+          {start && (
+            <Marker position={[start.lat, start.lng]} icon={startMarkerIcon}>
+              <Popup>Bắt đầu</Popup>
+            </Marker>
+          )}
+          {end && (
+            <Marker position={[end.lat, end.lng]} icon={endMarkerIcon}>
+              <Popup>Kết thúc</Popup>
+            </Marker>
+          )}
+          <GetLocation />
         </MapContainer>
       </div>
     </div>
